7427baa528af81c8f4cdb3dd3aa0b494b8bc74de
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2019-12-08 02:19:46 +0100

    Use ports from config service

--- apps/swapi/src/main.ts
+++ apps/swapi/src/main.ts
@@ -1,9 +1,10 @@
+import { ConfigService } from '@app/config/config.service';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(app.get(ConfigService).restPort);
 }
 bootstrap();
