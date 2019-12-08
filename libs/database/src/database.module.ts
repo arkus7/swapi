db30ed74588a277ec2cbafa17b5e4aba78dd282d
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2019-12-08 01:03:09 +0100

    Inject config service into database provider

--- libs/database/src/database.module.ts
+++ libs/database/src/database.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 
+import { ConfigModule } from '../../config/src/config.module';
 import { databaseProviders } from './database.providers';
 
 @Module({
+  imports: [ConfigModule],
   providers: [...databaseProviders],
   exports: [...databaseProviders],
 })
--- libs/database/src/database.providers.ts
+++ libs/database/src/database.providers.ts
@@ -1,14 +1,16 @@
 import * as mongoose from 'mongoose';
 
-const MONGODB_USER = 'swapi-dev-user';
-const MONGODB_PASSWORD = 'swapi-dev1';
+import { ConfigService } from '../../config/src/config.service';
 
 export const DATABASE_CONNECTION_TOKEN = 'DATABASE_CONNECTION';
 
 export const databaseProviders = [
   {
     provide: DATABASE_CONNECTION_TOKEN,
-    useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect(`mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@ds233198.mlab.com:33198/swapi-dev`),
+    useFactory: (config: ConfigService): Promise<typeof mongoose> => {
+      const { user, password, database } = config.mongoCredentials;
+      return mongoose.connect(`mongodb://${user}:${password}@ds233198.mlab.com:33198/${database}`, { useNewUrlParser: true });
+    },
+    inject: [ConfigService],
   },
 ];
