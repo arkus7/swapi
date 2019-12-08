63a9193b155f21279d057c66dddc14368b6ff48b
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2019-12-08 01:23:36 +0100

    Import film module in rest app

--- apps/swapi/src/app.module.ts
+++ apps/swapi/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FilmModule } from './film/film.module';
 
 @Module({
-  imports: [],
+  imports: [FilmModule],
   controllers: [AppController],
   providers: [AppService],
 })
