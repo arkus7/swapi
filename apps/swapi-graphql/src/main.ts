a796fe92ea9c81310907fbbad680bc99b29b41f4
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2019-12-08 11:37:47 +0100

    Log port on which the application works

--- apps/swapi-graphql/src/main.ts
+++ apps/swapi-graphql/src/main.ts
@@ -1,5 +1,5 @@
 import { ConfigService } from '@app/config/config.service';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
@@ -7,6 +7,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(app.get(ConfigService).graphqlPort);
+
+  const port = process.env.PORT || app.get(ConfigService).graphqlPort;
+  const logger = new Logger('Swapi GraphQL');
+  app.useLogger(logger);
+
+  await app.listen(port);
+  logger.log(`Swapi GraphQL listening on port ${port}`);
 }
 bootstrap();
