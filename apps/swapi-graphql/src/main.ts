bf5068a8b5a68cf8f5b55da0175342f6b4c5cbff
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2019-12-08 02:44:31 +0100

    Use validation pipe

--- apps/swapi-graphql/src/main.ts
+++ apps/swapi-graphql/src/main.ts
@@ -1,10 +1,12 @@
 import { ConfigService } from '@app/config/config.service';
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(app.get(ConfigService).graphqlPort);
 }
 bootstrap();
