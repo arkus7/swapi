f28257b2c33b5f85f929b3fe9244ee8ecd948226
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2019-12-09 22:34:31 +0100

    Add resolving home world in character model

--- apps/swapi-graphql/src/character/character.module.ts
+++ apps/swapi-graphql/src/character/character.module.ts
@@ -1,11 +1,12 @@
 import { CharacterDataModule } from '@app/database/characterData/characterData.module';
 import { FilmDataModule } from '@app/database/filmData/filmData.module';
+import { LocationDataModule } from '@app/database/locationData/locationData.module';
 import { Module } from '@nestjs/common';
 
 import { CharacterResolvers } from './character.resolvers';
 
 @Module({
-  imports: [CharacterDataModule, FilmDataModule],
+  imports: [CharacterDataModule, FilmDataModule, LocationDataModule],
   providers: [CharacterResolvers],
 })
 export class CharacterModule { }
