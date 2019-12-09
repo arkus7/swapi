0ea14120000fdca59c4d7db61dc3e21591563ef2
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2019-12-09 23:36:27 +0100

    Import location module in rest api

--- apps/swapi/src/app.module.ts
+++ apps/swapi/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CharacterModule } from './character/character.module';
 import { FilmModule } from './film/film.module';
+import { LocationModule } from './location/location.module';
 
 @Module({
-  imports: [FilmModule, CharacterModule],
+  imports: [FilmModule, CharacterModule, LocationModule],
   controllers: [AppController],
   providers: [AppService],
 })
