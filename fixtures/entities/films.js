a07e25566af29260d9a6eb243560e0c2b16bcfc4
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2019-12-09 02:19:52 +0100

    Update film model

--- fixtures/entities/films.js
+++ fixtures/entities/films.js
@@ -4,6 +4,7 @@ module.exports = [
   {
     _id: films.thePhantomMenace,
     "title": "Star Wars: Episode I The Phantom Menace",
+    episodeNumber: 1,
     "openingCrawl": "Turmoil has engulfed the\nGalactic Republic. The taxation\nof trade routes to outlying star\nsystems is in dispute.\n\nHoping to resolve the matter\nwith a blockade of deadly\nbattleships, the greedy Trade\nFederation has stopped all\nshipping to the small planet\nof Naboo.\n\nWhile the Congress of the\nRepublic endlessly debates\nthis alarming chain of events,\nthe Supreme Chancellor has\nsecretly dispatched two Jedi\nKnights, the guardians of\npeace and justice in the\ngalaxy, to settle the conflict....",
     "directors": [
       "George Lucas"
@@ -21,6 +22,7 @@ module.exports = [
   {
     _id: films.attackOfTheClones,
     "title": "Star Wars: Episode II Attack of the Clones",
+    episodeNumber: 2,
     "openingCrawl": "There is unrest in the Galactic\nSenate. Several thousand solar\nsystems have declared their\nintentions to leave the Republic.\n\nThis separatist movement,\nunder the leadership of the\nmysterious Count Dooku, has\nmade it difficult for the limited\nnumber of Jedi Knights to maintain\npeace and order in the galaxy.\n\nSenator Amidala, the former\nQueen of Naboo, is returning\nto the Galactic Senate to vote\non the critical issue of creating\nan ARMY OF THE REPUBLIC\nto assist the overwhelmed\nJedi....",
     "directors": [
       "George Lucas"
@@ -37,6 +39,7 @@ module.exports = [
   },
   {
     _id: films.revengeOfTheSith,
+    episodeNumber: 3,
     "title": "Star Wars: Episode III Revenge of the Sith",
     "openingCrawl": "War! The Republic is crumbling\nunder attacks by the ruthless\nSith Lord, Count Dooku.\nThere are heroes on both sides.\nEvil is everywhere.\n\nIn a stunning move, the\nfiendish droid leader, General\nGrievous, has swept into the\nRepublic capital and kidnapped\nChancellor Palpatine, leader of\nthe Galactic Senate.\n\nAs the Separatist Droid Army\nattempts to flee the besieged\ncapital with their valuable\nhostage, two Jedi Knights lead a\ndesperate mission to rescue the\ncaptive Chancellor....",
     "directors": [
@@ -55,6 +58,7 @@ module.exports = [
   {
     _id: films.newHope,
     "title": "Star Wars: Episode IV A New Hope",
+    episodeNumber: 4,
     "openingCrawl": "It is a period of civil war.\nRebel spaceships, striking\nfrom a hidden base, have won\ntheir first victory against\nthe evil Galactic Empire.\n\nDuring the battle, Rebel\nspies managed to steal secret\nplans to the Empire's\nultimate weapon, the DEATH\nSTAR, an armored space\nstation with enough power\nto destroy an entire planet.\n\nPursued by the Empire's\nsinister agents, Princess\nLeia races home aboard her\nstarship, custodian of the\nstolen plans that can save her\npeople and restore\nfreedom to the galaxy....",
     "directors": [
       "George Lucas"
@@ -73,6 +77,7 @@ module.exports = [
   {
     _id: films.empireStrikesBack,
     "title": "Star Wars: Episode V The Empire Strikes Back",
+    episodeNumber: 5,
     "openingCrawl": "It is a dark time for the\nRebellion. Although the Death\nStar has been destroyed,\nImperial troops have driven the\nRebel forces from their hidden\nbase and pursued them across\nthe galaxy.\n\nEvading the dreaded Imperial\nStarfleet, a group of freedom\nfighters led by Luke Skywalker\nhas established a new secret\nbase on the remote ice world\nof Hoth.\n\nThe evil lord Darth Vader,\nobsessed with finding young\nSkywalker, has dispatched\nthousands of remote probes into\nthe far reaches of space....",
     "directors": [
       "Irvin Kershner"
@@ -91,6 +96,7 @@ module.exports = [
   {
     _id: films.returnOfTheJedi,
     "title": "Star Wars: Episode VI Return of the Jedi",
+    episodeNumber: 6,
     "openingCrawl": "Luke Skywalker has returned to\nhis home planet of Tatooine in\nan attempt to rescue his\nfriend Han Solo from the\nclutches of the vile gangster\nJabba the Hutt.\n\nLittle does Luke know that the\nGALACTIC EMPIRE has secretly\nbegun construction on a new\narmored space station even\nmore powerful than the first\ndreaded Death Star.\n\nWhen completed, this ultimate\nweapon will spell certain doom\nfor the small band of rebels\nstruggling to restore freedom\nto the galaxy...",
     "directors": [
       "Richard Marquand"
@@ -110,6 +116,7 @@ module.exports = [
   {
     _id: films.forceAwakens,
     "title": "Star Wars: Episode VII The Force Awakens",
+    episodeNumber: 7,
     "openingCrawl": "Luke Skywalker has vanished.\nIn his absence, the sinister\nFIRST ORDER has risen from\nthe ashes of the Empire\nand will not rest until\nSkywalker, the last Jedi,\nhas been destroyed.\n\nWith the support of the\nREPUBLIC, General Leia Organa\nleads a brave RESISTANCE.\nShe is desperate to find her\nbrother Luke and gain his\nhelp in restoring peace\nand justice to the galaxy.\n\nLeia has sent her most daring\npilot on a secret mission\nto Jakku, where an old ally\nhas discovered a clue to\nLuke's whereabouts....",
     "directors": [
       "J.J. Abrams"
@@ -131,6 +138,7 @@ module.exports = [
   {
     _id: films.lastJedi,
     "title": "Star Wars: Episode VIII The Last Jedi",
+    episodeNumber: 8,
     "openingCrawl": "The FIRST ORDER reigns.\nHaving decimated the peaceful\nRepublic, Supreme Leader Snoke\nnow deploys his merciless\nlegions to seize military\ncontrol of the galaxy.\n\nOnly General Leia Organa's\nband of RESISTANCE fighters\nstand against the rising\ntyranny, certain that Jedi\nMaster Luke Skywalker will\nreturn and restore a spark of\nhope to the fight.\n\nBut the Resistance has been\nexposed. As the First Order\nspeeds toward the Rebel base,\nthe brave heroes mount a\ndesperate escape....",
     "directors": [
       "Rian Johnson"
