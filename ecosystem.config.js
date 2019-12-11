cc27cee0636e448193ea7ac73e8a8d0b9e8a8d60
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2019-12-11 21:14:26 +0100

    Add ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  apps: [{
+    name: 'swapi-rest',
+    script: 'dist/apps/swapi/main.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development',
+      MONGODB_URI: 'mongodb://swapi-dev-user:swapi-dev1@ds233198.mlab.com:33198/swapi-dev',
+      PORT: 3002
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  },
+  {
+    name: 'swapi-graphql',
+    script: 'dist/apps/swapi-graphql/main.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development',
+      PORT: 3001,
+      MONGODB_URI: 'mongodb://swapi-dev-user:swapi-dev1@ds233198.mlab.com:33198/swapi-dev',
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy: {
+    production: {
+      user: 'root',
+      host: '167.172.109.84',
+      ref: 'origin/master',
+      repo: 'git@github.com:arkus7/swapi.git',
+      path: '/var/www/production',
+      'post-deploy': 'npm install && nest build && nest build swapi-graphql && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
