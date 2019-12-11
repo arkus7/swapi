e1c7f82d40acdd8ef5dab174d332acbb4978f7eb
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2019-12-11 21:14:12 +0100

    Fix starting rest api

--- apps/swapi/src/main.ts
+++ apps/swapi/src/main.ts
@@ -1,5 +1,5 @@
 import { ConfigService } from '@app/config/config.service';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
@@ -8,6 +8,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  await app.listen(app.get(ConfigService).restPort);
+  const port = process.env.PORT || app.get(ConfigService).restPort;
+  const logger = new Logger('Swapi REST');
+  app.useLogger(logger);
+
+  await app.listen(port);
+  logger.log(`Swapi REST listening on port ${port}`);
 }
 bootstrap();
